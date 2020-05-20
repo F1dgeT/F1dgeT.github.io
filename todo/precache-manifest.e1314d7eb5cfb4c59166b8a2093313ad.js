0815eca1b0b64a4ee5bfb78e2102ac47ac59a451
F1dgeT <64103347+F1dgeT@users.noreply.github.com>
2020-05-20 15:53:46 +0300

    Add files via upload

---
+++ todo/precache-manifest.e1314d7eb5cfb4c59166b8a2093313ad.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5ce18efffce7ee354786b73ed5a3ef01",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2e4261d591ea80c54f09",
+    "url": "/static/css/main.e2c9528b.chunk.css"
+  },
+  {
+    "revision": "34a4b5f4f44a23d8374f",
+    "url": "/static/js/2.1fbb5c08.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/static/js/2.1fbb5c08.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2e4261d591ea80c54f09",
+    "url": "/static/js/main.b1051f6d.chunk.js"
+  },
+  {
+    "revision": "e5178c0459ac74718576",
+    "url": "/static/js/runtime-main.d7982416.js"
+  }
+]);
